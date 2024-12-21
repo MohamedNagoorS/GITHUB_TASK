efc5d3193b488d4d68359d1f0ee02e4bbc90e28b
Mohamed Nagoor <211001059@rajalakshmi.edu.in>
2024-12-21 20:50:23 +0530

    Changed to iterative method

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,10 @@
 def fibonacci(n):
     if n <= 1:
         return n
-    return fibonacci(n-1) + fibonacci(n-2)
-n =int(input("Enter n"))
+    a, b = 0, 1
+    for i in range(2, n + 1):
+        a, b = b, a + b
+    return b
+n=int(input("Enter n"))
 print(f"The {n}th Fibonacci number is: {fibonacci(n)}")
+
